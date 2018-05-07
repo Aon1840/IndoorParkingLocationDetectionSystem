53aa259d99220865049f5a1a97b5cd81f920cbcd
Aon1840 <aonattapon1840.ap@gmail.com>
2018-05-07 13:45:08 +0700

    Add interface onNetworkCallback for response data

---
+++ app/src/main/java/com/example/aon_attapon/indoorparkinglocationdetectionsystem/Callback/onNetworkCallbackListner.java
@@ -0,0 +1,13 @@
+package com.example.aon_attapon.indoorparkinglocationdetectionsystem.Callback;
+
+import com.example.aon_attapon.indoorparkinglocationdetectionsystem.Model.CarPosition;
+
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+
+public interface onNetworkCallbackListner {
+    public void onResponse(CarPosition carPosition, Retrofit retrofit);
+    public void onBodyError(ResponseBody responseBodyError);
+    public void onBodyErrorIsNull();
+    public void onFailure(Throwable t);
+}
