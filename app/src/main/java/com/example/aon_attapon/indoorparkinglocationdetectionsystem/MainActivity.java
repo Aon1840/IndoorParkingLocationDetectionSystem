bc0046d2829832b9443e9f0e7fb3cbf23c8d3cc7
Aon1840 <aonattapon1840.ap@gmail.com>
2018-05-07 13:20:45 +0700

    Add view in activity_main

--- app/src/main/java/com/example/aon_attapon/indoorparkinglocationdetectionsystem/MainActivity.java
+++ app/src/main/java/com/example/aon_attapon/indoorparkinglocationdetectionsystem/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.aon_attapon.indoorparkinglocationdetectionsystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+//    public static final String EXTRA_MESSAGE = ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+//        Intent intent = new
     }
 }
