b66e1845a85bde4e301d6364293828852fae31eb
Aon1840 <aonattapon1840.ap@gmail.com>
2018-05-08 16:01:08 +0700

    Add path url for user in ParkingService interface

--- app/src/main/java/com/example/aon_attapon/indoorparkinglocationdetectionsystem/API/ParkingService.java
+++ app/src/main/java/com/example/aon_attapon/indoorparkinglocationdetectionsystem/API/ParkingService.java
@@ -1,12 +1,16 @@
 package com.example.aon_attapon.indoorparkinglocationdetectionsystem.API;
 
 import com.example.aon_attapon.indoorparkinglocationdetectionsystem.Model.CarPosition;
+import com.example.aon_attapon.indoorparkinglocationdetectionsystem.Model.User;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface ParkingService {
-    @GET("/carPosition/{user_id}")
+    @GET("/username/{user_id}")
     Call<CarPosition> getCarPositon(@Path("user_id") int user_id);
+
+    @GET("/users/{user_id}")
+    Call<User> getUser(@Path("user_id") int user_id);
 }
